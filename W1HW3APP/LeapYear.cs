a8c522941a7cbbe676815fb02d5ddccd6cba03eb
JMY <9294759+J18279899751@user.noreply.gitee.com>
2022-02-26 12:42:01 +0800

    添加项目文件。

---
+++ W1HW3APP/LeapYear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW3APP
+{
+    internal class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Enter year");
+            year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+            Console.WriteLine("{0}  is leap year",year);
+            else
+                Console.WriteLine("{0} is not a leap year", year);
+        }
+    }
+}
